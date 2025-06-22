ee787ccf5cebec7d2c0cc96c4d80edb13de85554
keith-maura <keith.m@northeastern.edu>
2025-06-22 19:14:16 -0400

    feat(ch6): do chapter

---
+++ chapter5/main.py
@@ -0,0 +1,145 @@
+"""FastAPI program - Chapter 4"""
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlalchemy.orm import Session
+from datetime import date
+
+import crud, schemas
+from database import SessionLocal
+
+api_description = """
+This API provides read-only access to info from the SportsWorldCentral (SWC) Fantasy Football API.
+The endpoints are grouped into the following categories:
+
+## Analytics
+Get information about the health of the API and counts of leagues, teams, and players
+ 
+## Player
+You can get a list of NFL players, or search for an individual player by player_id.
+
+## Scoring
+You can get a list of NFL player performances, including the fantasy points they scored using SWC league scoring.
+
+## Membership
+Get information about all the SWC fantasy football leagues and the teams in them.
+"""
+
+app = FastAPI(
+    description=api_description,
+    title="Sports World Central (SWC) Fantasy Football API",
+    version = "0.1"
+)
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/",
+         tags=["analytics"])
+async def root():
+    return {"message": "API health check successful"}
+
+@app.get("/v0/players/{player_id}",
+         response_model=schemas.Player,
+         summary="Get one player using the Player ID, which is internal to SWC",
+         description="If you have an SWC player ID of a player from another API call such as v0_get_players, you can call this API using the player ID",
+         response_description="One NFL player",
+         operation_id="v0_get_players_by_player_id",
+         tags=["player"])
+def read_player(player_id: int, db: Session=Depends(get_db)):
+    player = crud.get_player(db, player_id)
+    if player is None:
+        raise HTTPException(status_code=404, detail="Player not found")
+    return player
+    
+@app.get("/v0/players/",
+         response_model=list[schemas.Player],
+         summary="Get players matching any given filters",
+         description="Get a list of all players in SWC data according to input filters",
+         response_description="List of NFL players",
+         operation_id="v0_get_players",
+         tags=["player"])
+def read_players(skip: int=Query(0, description="The number of items to skip at the beginning of the API call."),
+                 limit: int=Query(100, description="The number of records to return after the skipped records."),
+                 minimum_last_changed_date: date=Query(None, description="The minimum date of change that you want to return records. Exclude any records changed before this."),
+                 last_name: str=Query(None, description="The last name of the players to return."),
+                 first_name: str=Query(None, description="The first name of the players to return."),
+                 db: Session=Depends(get_db)):
+    players = crud.get_players(db, skip=skip, limit=limit, minimum_last_changed_date=minimum_last_changed_date, last_name=last_name, first_name=first_name)
+    return players
+    
+@app.get("/v0/performances/",
+         response_model=list[schemas.Performance],
+         summary="Get performances matching any given filters",
+         description="Get a list of all performances in SWC data according to input filters",
+         response_description="List of NFL performances",
+         operation_id="v0_get_performances",
+         tags=["scoring"])
+def read_performances(skip: int=Query(0, description="The number of items to skip at the beginning of the API call."),
+                                      limit: int=Query(100, description="The number of records to return after the skipped records."),
+                                      minimum_last_changed_date: date=Query(None, description="The minimum date of change that you want to return records. Exclude any records changed before this."),
+                                      db: Session=Depends(get_db)):
+    performances = crud.get_performances(db, skip=skip, limit=limit, minimum_last_changed_date=minimum_last_changed_date)
+    return performances
+
+@app.get("/v0/leagues/{league_id}",
+         response_model=schemas.League,
+         summary="Get one league using the League ID, which is internal to SWC",
+         description="If you have an SWC league ID of a player from another API call such as v0_get_leagues, you can call this API using the league ID",
+         response_description="One NFL league",
+         operation_id="v0_get_leagues_by_league_id",
+         tags=["membership"])
+def read_league(league_id: int, db: Session=Depends(get_db)):
+    league = crud.get_league(db, league_id=league_id)
+    if league is None:
+        raise HTTPException(status_code=404, detail="League not found")
+    return league
+
+@app.get("/v0/leagues/",
+         response_model=list[schemas.League],
+         summary="Get a list of all leagues in SWC data according to input filters",
+         description="Get a list of all leagues in SWC data according to input filters",
+         response_description="List of NFL leagues",
+         operation_id="v0_get_leagues",
+         tags=["membership"])
+def read_leagues(skip: int=Query(0, description="The number of items to skip at the beginning of the API call."),
+                                 limit: int=Query(100, description="The number of records to return after the skipped records."),
+                                 minimum_last_changed_date: date=Query(None, description="The minimum date of change that you want to return records. Exclude any records changed before this."),
+                                 league_name: str=Query(None, description="The name of the leagues to return."),
+                                 db: Session=Depends(get_db)):
+    leagues = crud.get_leagues(db, skip=skip, limit=limit, minimum_last_changed_date=minimum_last_changed_date, league_name=league_name)
+    return leagues
+
+@app.get("/v0/teams/",
+         response_model=list[schemas.Team],
+         summary="Get a list of all teams in SWC data according to input filters",
+         description="Get a list of all teams in SWC data according to input filters",
+         response_description="List of NFL teams",
+         operation_id="v0_get_teams",
+         tags=["membership"])
+def read_teams(skip: int=Query(0, description="The number of items to skip at the beginning of the API call."),
+                               limit: int=Query(100, description="The number of records to return after the skipped records."),
+                               minimum_last_changed_date: date=Query(None, description="The minimum date of change that you want to return records. Exclude any records changed before this."),
+                               team_name: str=Query(None, description="The name of the teams to return."),
+                               league_id: int=Query(None, description="The ID of the league for the teams to return"),
+                               db: Session=Depends(get_db)):
+    teams = crud.get_teams(db, skip=skip, limit=limit, minimum_last_changed_date=minimum_last_changed_date, team_name=team_name, league_id=league_id)
+    return teams
+
+@app.get("/v0/counts/",
+         response_model=schemas.Counts,
+         summary="Get count summary statistics",
+         description="Get the count of leagues, teams, and players",
+         response_description="List of counts",
+         operation_id="v0_get_counts",
+         tags=["analytics"])
+def get_count(db: Session=Depends(get_db)):
+    counts = schemas.Counts(
+        league_count=crud.get_league_count(db),
+        team_count=crud.get_team_count(db),
+        player_count=crud.get_player_count(db)
+    )
+    return counts
---
+++ chapter6/crud.py
@@ -0,0 +1,65 @@
+"""SQLAlchemy query functions"""
+
+from sqlalchemy.orm import Session, joinedload
+from datetime import date
+
+import models
+
+def get_player(db: Session, player_id: int):
+    return db.query(models.Player).filter(models.Player.player_id == player_id).first()
+
+def get_players(db: Session, skip: int=0, limit: int=100, minimum_last_changed_date: date=None, last_name: str=None, first_name: str=None):
+    query = db.query(models.Player)
+    if minimum_last_changed_date:
+        query = query.filter(models.Player.last_changed_date >= minimum_last_changed_date)
+    if last_name:
+        query = query.filter(models.Player.last_name == last_name)
+    if first_name:
+        query = query.filter(models.Player.first_name == first_name)
+    return query.offset(skip).limit(limit).all()
+
+def get_performances(db: Session, skip: int=0, limit: int=100, minimum_last_changed_date: date=None):
+    query = db.query(models.Performance)
+    if minimum_last_changed_date:
+        query = query.filter(models.Performance.last_changed_date >= minimum_last_changed_date)
+    return query.offset(skip).limit(limit).all()
+
+def get_league(db :Session, league_id: int=None):
+    query = db.query(models.League)
+    if league_id:
+        query = query.filter(models.League.league_id == league_id)
+    return query.first()
+
+def get_leagues(db :Session, skip: int=0, limit: int=100, minimum_last_changed_date: date=None, league_name: str=None):
+    query = db.query(models.League).options(joinedload(models.League.teams))
+    if minimum_last_changed_date:
+        query = query.filter(models.League.minimum_last_changed_date >= minimum_last_changed_date)
+    if league_name:
+        query = query.filter(models.League.league_name == league_name)
+    return query.offset(skip).limit(limit).all()
+
+def get_teams(db: Session, skip: int=0, limit: int=100, minimum_last_changed_date: date=None, team_name: str=None, league_id: int=None):
+    query = db.query(models.Team)
+    if minimum_last_changed_date:
+        query = query.filter(models.Team.last_changed_date >= minimum_last_changed_date)
+    if team_name:
+        query = query.filter(models.Team.team_name == team_name)
+    if league_id:
+        query = query.filter(models.Team.league_id == league_id)
+    return query.offset(skip).limit(limit).all()
+
+# Analytics queries
+def get_player_count(db: Session):
+    query = db.query(models.Player)
+    return query.count()
+
+def get_team_count(db: Session):
+    query = db.query(models.Team)
+    return query.count()
+
+def get_league_count(db: Session):
+    query = db.query(models.League)
+    return query.count()
+
+
+    
